78f8642a2397516dadaae3ce1365c9eb505a6a18
Nawnit kumar <nawnitk2006@gmail.com>
2026-07-09 19:54:13 +0530

    Time: 0 ms (100%), Space: 31.4 MB (7.4%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -3,7 +3,8 @@ public:
     int search(vector<int>& nums, int target) {
        int start=0;
        int end=nums.size()-1;
-       int mid=start+(end-start)/2;
+       int mid=start+(end-start)/2; 
+       //when we write mid=(start+end)/2 then TLE if two maximum integer added then out of range of an interger 
     while(start<=end){
         if(nums[mid]==target){
             return mid;
