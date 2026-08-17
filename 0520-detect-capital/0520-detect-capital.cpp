e91047e46e3f3bdffd223742887047852fe0254d
Nawnit kumar <nawnitk2006@gmail.com>
2026-08-18 01:08:37 +0530

    Time: 0 ms (100%), Space: 8.3 MB (15.55%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+       int count=0;
+        int n = word.length();
+       for(int i=0;i<n;i++){
+         
+        if(word[i]>='A' && word[i]<='Z'){
+            count++;
+        }
+       } 
+       if(count==n || count==0){
+        return true;
+       }
+       if( count==1 && (word[0]>='A' && word[0]<='Z')){
+        return true;
+       }
+       return false;
+    }
+};
